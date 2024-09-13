1659737606df9d812e007a08eb06898977551370
Codered20 <anuj201982@gmail.com>
2024-09-14 00:36:42 +0530

    added entities within room and booked room models

--- Backend/src/main/java/com/summit_suites/model/Room.java
+++ Backend/src/main/java/com/summit_suites/model/Room.java
@@ -1,5 +1,56 @@
 package com.summit_suites.model;
 
-public class Room {
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class Room {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String roomType;
+	private BigDecimal roomPrice;
+	private boolean isBooked = false;
+	@Lob
+	private Blob photo;
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Booked_Rooms> bookings;
+
+	public Room() {
+		super();
+		this.bookings = new ArrayList<>();
+	}
+	
+	public void addBookings(Booked_Rooms booking) {
+		bookings.add(booking);
+		booking.setRoom(this);
+		isBooked = true;
+		String bookingCode = RandomStringUtils.randomNumeric(10);
+		booking.setBookingConfirmationCode(bookingCode);
+		
+	}
+	
+	
 }
--- Backend/src/main/java/com/summit_suites/service/BookedRoomServiceImpl.java
+++ Backend/src/main/java/com/summit_suites/service/BookedRoomServiceImpl.java
@@ -1,5 +1,5 @@
 package com.summit_suites.service;
 
 public class BookedRoomServiceImpl {
-	int rooms = 0;
+	
 }
